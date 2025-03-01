5af5144fd1e4a2014af06d634ce16f55fcf746fb
Lucas Kazuhiro Okokama <lucaslko429@gmail.com>
2025-02-28 21:31:45 -0300

    learn(ap03-react): encerra a apostila

---
+++ primeiro-app/src/App.jsx
@@ -0,0 +1,52 @@
+const App = () => {
+  const estilosBotao = {
+        marginTop: 12,
+        paddingTop: 8,
+        paddingBottom: 8,
+        backgroundColor: "rebeccapurple",
+        color: "white",
+        border: "none",
+        width: "100%",
+        borderRadius: 8,
+  }
+  const textoDoRotulo = "Nome"
+  const obterTextoDoBotao = () => {return "Enviar"}
+  const aoCliclar = () => alert("Clicou")
+
+  return (
+    <div
+      style={{
+        width: 768,
+        margin: "auto",
+        backgroundColor: "#EEE",
+        padding: 12,
+        borderRadius: 8,
+      }}
+    >
+      <label
+        className="rotulo"
+        htmlFor="nome"
+        style={{ display: "block", marginBottom: 4 }}
+      >
+        {textoDoRotulo}
+      </label>
+      <input
+        type="text"
+        id="nome"
+        style={{
+          paddingTop: 8,
+          paddingBottom: 8,
+          borderStyle: "hidden",
+          width: "100%",
+          borderRadius: 8,
+          outline: "none",
+        }}
+      />
+      <button onClick={() => aoCliclar()} style={estilosBotao}>
+        {obterTextoDoBotao()}
+      </button>
+    </div>
+  );
+};
+
+export default App;
